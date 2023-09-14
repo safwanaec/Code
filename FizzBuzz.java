628113e201738e0447d2d2e8aadbf86580849797
safwanaec <ashraisafwan@gmail.com>
2023-09-14 16:52:01 +0600

    14th September 2023

---
+++ FizzBuzz.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int num = inp.nextInt();
+
+        if(num%5==0 && num%3==0){
+            System.out.println("FizzBuzz");
+        }
+        else if(num%5==0){
+            System.out.println("Fizz");
+        }
+        else if(num%3==0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(num);
+        }
+
+        inp.close();
+        
+    }
+}
--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -5,13 +5,37 @@ public class MortgageCalculator {
     public static void main(String[] args) {
         
         Scanner inp = new Scanner(System.in);
-        System.out.print("Please enter Capital amount: ");
-        int capital = inp.nextInt();
-        System.out.print("\nPlease enter rate of interest: ");
-        float anualInterest = inp.nextFloat();
-        float monthlyInterest = anualInterest/1200;
-        System.out.print("\nPlease enter time in years: ");
-        float time = inp.nextFloat();
+        int capital;
+        float anualInterest, monthlyInterest, time;
+
+        while(true){
+            System.out.print("Please enter Capital amount: ");
+            capital = inp.nextInt();
+            if(capital>1000000 || capital<1000){
+                System.out.println("Please enter amount between 1K$ and 1M$");
+            }
+            else break;
+        }
+
+        while(true){
+            System.out.print("\nPlease enter rate of interest: ");
+            anualInterest = inp.nextFloat();
+            if(anualInterest>30 || anualInterest<=0){
+                System.out.println("Please enter interest between 0 to 30 percent");
+            }
+            else break;
+        }
+        
+        monthlyInterest = anualInterest/1200;
+
+        while(true){
+            System.out.print("\nPlease enter time in years: ");
+            time = inp.nextFloat();
+            if(time>30 || time<=0){
+                System.out.println("Please enter time between 0 to 30 years");
+            }
+            else break;
+        }
 
         float mortgage = (((float)capital*monthlyInterest*(float)Math.pow(1+monthlyInterest,time*12))/((float)Math.pow(1+monthlyInterest,time*12)-1.0F));
 
